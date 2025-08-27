ea59b0ea7398944f227340ecd0811390208c5afe
HaimKatav <51018133+HaimKatav@users.noreply.github.com>
2025-08-28 01:04:22 +0300

    Removed all previous code starting fresh

--- Assets/Scripts/Core/Data/GameEndResultData.cs
+++
@@ -1,9 +0,0 @@
-namespace CardWar.Core.Data
-{
-    public class GameEndResultData
-    {
-        public bool PlayerWon { get; set; }
-        public int TotalRounds { get; set; }
-        public string WinnerName => PlayerWon ? "Player" : "Opponent";
-    }
-}
